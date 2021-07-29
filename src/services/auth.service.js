45cae3f94915397bc0990116f66b213bcc472a6e
Stone Tech <jsreputation@gmail.com>
2021-07-29 09:59:57 +0300

    feat: auth service

---
+++ src/services/auth.service.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api/auth/";
+
+class AuthService {
+
+    signIn(username, password) {
+        return axios.post(API_URL + 'signin', { username, password })
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem('user', JSON.stringify(response.data));
+            }
+            return response.data;
+        });
+    }
+
+    signOut() {
+        localStorage.removeItem('user');
+    }
+
+    signUp(username, email, password) {
+        return axios.post(API_URL + 'signup', {
+            username, email, password
+        })
+    }
+}
+
+export default new AuthService();
