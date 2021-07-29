013fb305b121408458224556aa2e0d2bad74e1d8
Stone Tech <jsreputation@gmail.com>
2021-07-29 11:22:32 +0300

    add: action types

---
+++ src/actions/type.js
@@ -0,0 +1,8 @@
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const SIGNIN_SUCCESS = "SIGNIN_SUCCESS";
+export const SIGNIN_FAIL = "SIGNIN_FAIL";
+export const SIGNOUT = "SIGNOUT";
+
+export const SET_MESSAGE = "SET_MESSAGE";
+export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
